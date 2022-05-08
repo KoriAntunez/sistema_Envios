b861567950775c3907f6cf05aa353682711b990f
KoriAntunez <kori.antunez@unmsm.edu.pe>
2022-05-08 00:16:18 -0500

    commit consultar

--- src/pages/Consultar.js
+++ src/pages/Consultar.js
@@ -1,16 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Form, Col, Table } from "react-bootstrap";
+import { Container, Row, Form, Col, Table, FormControl, Button } from "react-bootstrap";
 import Navbar from "../components/navbar";
-<<<<<<< Updated upstream
 const { Column } = Table;
-
-
-
-
-
-=======
->>>>>>> Stashed changes
-
 const Consultar = () => {
     return(
         <>
@@ -20,7 +11,53 @@ const Consultar = () => {
                 <h1>Consultar Envio</h1>
                 <h4>Consulte por su Cédula o número de orden de compra</h4>
             </Row>
-      </Container>
+        </Container>
+            <Form>
+              <Row>
+                <Col xs={6}>
+                  <Form.Label>Cedula</Form.Label>
+                   <Row>
+                    <Col xs={10}>
+                      <FormControl
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                      />
+                   </Col>
+                   <Col xs={2}>               
+                    <Button variant="outline-success">Search</Button>
+                   </Col>
+                  </Row>                
+                </Col>
+                <Col xs={6}>
+                  <Form.Label>Código de envio</Form.Label>
+                   <Row>
+                    <Col xs={10}>
+                      <FormControl
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                      />
+                   </Col>
+                   <Col xs={2}>               
+                    <Button variant="outline-success">Search</Button>
+                   </Col>
+                  </Row>                
+                </Col>
+              </Row>
+            </Form>
+            <Table striped bordered hover className="mt-5">
+              <thead>
+                  <tr>
+                  <th>#</th>
+                  <th>Nombre producto</th>
+                  <th>Precio</th>
+                  <th>Fecha Act</th>
+                  </tr>
+              </thead>
+            </Table>       
         </> 
         );
 }
